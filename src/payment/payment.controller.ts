b95b749ec5b3e78704845ed108034d3600bc72b9
Nguyễn Nam <nguyennam@nam.local>
2023-10-06 18:18:44 +0700

    fix: remove some console log. Remove 2 apis in user controller which is used by admin

--- src/payment/payment.controller.ts
+++ src/payment/payment.controller.ts
@@ -6,21 +6,33 @@ import {
   Param,
   Post,
   Query,
+  UseGuards,
 } from '@nestjs/common';
-import { ApiBody, ApiParam, ApiQuery, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOperation,
+  ApiParam,
+  ApiQuery,
+  ApiTags,
+} from '@nestjs/swagger';
 import { PaymentService } from './payment.service';
 import { ResponseInterface } from 'src/cores/response.interface';
 import { PaymentCreateDTO, PaymentDTO } from './payment.dto';
 import { getInfoDataForArray } from 'src/utils';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @ApiTags('payments')
+@ApiBearerAuth()
+@UseGuards(AuthGuard)
 @Controller('payments')
 export class PaymentController {
   constructor(
     @Inject(PaymentService) private readonly paymentService: PaymentService,
   ) {}
 
-  @Get()
+  @Post()
+  @ApiOperation({ summary: 'Get all payments' })
   @ApiQuery({ name: 'page', type: Number, required: false })
   @ApiQuery({ name: 'total', type: Number, required: false })
   async findAll(
@@ -43,7 +55,8 @@ export class PaymentController {
     };
   }
 
-  @Post()
+  @Post('create')
+  @ApiOperation({ summary: 'Create new payment' })
   @ApiBody({ type: PaymentCreateDTO })
   async create(@Body() data: PaymentCreateDTO): Promise<ResponseInterface> {
     return {
@@ -52,11 +65,12 @@ export class PaymentController {
     };
   }
 
-  @Post(':idUser')
+  @Post('create/:idUser')
   @ApiParam({
     name: 'idUser',
     description: 'Id of user',
   })
+  @ApiOperation({ summary: 'Create new payment for user' })
   @ApiBody({ type: PaymentCreateDTO })
   async createPayment(
     @Param('idUser') idUser: string,
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -9,9 +9,14 @@ import {
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserDTO } from './user.dto';
-import { ApiBearerAuth, ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { AuthGuard } from '../auth/auth.guard';
-import { RolesGuard } from 'src/auth/role/roles.guard';
 import { Auth } from '../auth/auth.decorator';
 import { Role } from '../enums/role.enum';
 import { ResponseInterface } from '../cores/response.interface';
@@ -19,37 +24,11 @@ import { ResponseInterface } from '../cores/response.interface';
 @ApiTags('users')
 @ApiBearerAuth()
 @Controller('users')
+@UseGuards(AuthGuard)
 export class UserController {
   constructor(@Inject(UserService) private userSerice: UserService) {}
-
-  @Get()
-  async findAll(): Promise<any> {
-    return {
-      status: 200,
-      metada: await this.userSerice.findAll(),
-    };
-  }
-
-  @Post()
-  @ApiResponse({
-    status: 201,
-    description: 'The record has been successfully created.',
-  })
-  @ApiResponse({ status: 403, description: 'Forbidden.' })
-  @ApiBody({
-    description: 'List of cats',
-    type: UserDTO,
-  })
-  @Auth(Role.Admin) // bao gom check token va check role
-  @UseGuards(AuthGuard) // chi check token
-  async create(@Body() data: UserDTO): Promise<ResponseInterface> {
-    return {
-      status: 200,
-      metadata: await this.userSerice.create(data),
-    };
-  }
-
-  @Get('posts/:id')
+  @Post('posts/:id')
+  @ApiOperation({ summary: 'Get posts by user id' })
   async getPostsByUserId(@Param('id') id: string): Promise<ResponseInterface> {
     return {
       status: 200,
